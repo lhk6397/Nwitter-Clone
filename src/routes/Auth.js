5be4162ea7967e191982e83727b95dc2c39850bd
maruhxn <a23686397@gmail.com>
2022-08-04 03:54:45 +0900

    Login Form

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const onChange = (event) => {
+    const {
+      target: { name, value },
+    } = event;
+    if (name === "email") {
+      setEmail(value);
+    } else if (name === "password") {
+      setPassword(value);
+    }
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+  };
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          value={email}
+          type="text"
+          placeholder="Email"
+          required
+          onChange={onChange}
+        />
+        <input
+          value={password}
+          type="password"
+          placeholder="Password"
+          required
+          onChange={onChange}
+        />
+        <input type="submit" value="Log In" />
+      </form>
+      <div>
+        <button>Continue with Google</button>
+        <button>Continue with Github</button>
+      </div>
+    </>
+  );
 };
 
 export default Auth;
