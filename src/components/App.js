05579e73267052c3a65cb77c54fad0afdf250bbe
maruhxn <a23686397@gmail.com>
2022-08-04 03:39:20 +0900

    Setup Firebase Auth

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
 import Router from "./Router";
+import { authService } from "../fbase";
 
 function App() {
-  return <Router />;
+  const user = authService.currentUser;
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  console.log(user);
+  return (
+    <>
+      <Router isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
 }
 
 export default App;
--- src/fbase.js
+++ src/fbase.js
@@ -1,4 +1,6 @@
-import { initializeApp } from "firebase/app";
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
@@ -9,5 +11,5 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_APP_ID,
 };
 
-const app = initializeApp(firebaseConfig);
-export default app;
+firebase.initializeApp(firebaseConfig);
+export const authService = getAuth();
